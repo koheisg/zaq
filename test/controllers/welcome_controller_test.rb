5a482e32a49eb2ab0ccbfca01f7835b02a5639e1
Kohei SUGI <kohei.sg@gmail.com>
2020-10-07 21:57:37 +0900

    Change root to welcome#show

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get welcome_show_url
+    get root_url
     assert_response :success
   end
 
