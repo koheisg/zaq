ab3d4ba156f20b8604b501a4e05c07490bd3eb03
Kohei SUGI <kohei.sg@gmail.com>
2020-10-10 03:08:53 +0900

    tmp

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  namespace :questions do
-    get 'answers/create'
+  resources :questions do
+    resources :answers
   end
-  resources :answers
-  resources :questions
   root to: 'welcome#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
