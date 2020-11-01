0887ee928e6775cbef8c0e0259e5a289571eb11e
Kohei SUGI <kohei.sg@gmail.com>
2020-11-01 15:08:38 +0900

    Add user_id on user_authentications

--- db/migrate/20201101043105_create_user_authentications.rb
+++ db/migrate/20201101043105_create_user_authentications.rb
@@ -2,6 +2,7 @@ class CreateUserAuthentications < ActiveRecord::Migration[6.0]
   def change
     create_table :user_authentications do |t|
       t.references :authenticationable, polymorphic: true, null: false, index: {name: "authenticationable"}
+      t.references :user, null: false
 
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -70,9 +70,11 @@ ActiveRecord::Schema.define(version: 2020_11_01_043105) do
   create_table "user_authentications", force: :cascade do |t|
     t.string "authenticationable_type", null: false
     t.integer "authenticationable_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["authenticationable_type", "authenticationable_id"], name: "authenticationable"
+    t.index ["user_id"], name: "index_user_authentications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
