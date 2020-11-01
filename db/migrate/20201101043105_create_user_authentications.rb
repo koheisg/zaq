3b3f7ad39c6f26c3c1f2a25c0184e0159c134806
Kohei SUGI <kohei.sg@gmail.com>
2020-11-01 13:35:54 +0900

    Add index name

--- db/migrate/20201101043105_create_user_authentications.rb
+++ db/migrate/20201101043105_create_user_authentications.rb
@@ -1,7 +1,8 @@
 class CreateUserAuthentications < ActiveRecord::Migration[6.0]
   def change
     create_table :user_authentications do |t|
-      t.references :authenticationable, polymorphic: true, null: false
+      t.references :authenticationable, polymorphic: true, null: false, index: {name: "authenticationable"}
+
 
       t.timestamps
     end
